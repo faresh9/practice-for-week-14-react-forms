501be8c050c88d40fe19fb3c8a985afec2f8f9b6
faresh9 <93577791+faresh9@users.noreply.github.com>
2023-08-22 17:23:19 +0300

    Handling form submissions

--- src/ContactUs/index.js
+++ src/ContactUs/index.js
@@ -1,23 +1,41 @@
+// ./src/ContactUs.js
 import { useState } from "react";
+
 function ContactUs() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+
   return (
     <div>
       <h2>Contact Us</h2>
       <form>
         <div>
           <label htmlFor="name">Name:</label>
-          <input id="name" type="text" input={name} />
+          <input
+            id="name"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
         </div>
         <div>
           <label htmlFor="email">Email:</label>
-          <input id="email" type="text" input={email} />
+          <input
+            id="email"
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
         </div>
         <div>
           <label htmlFor="phone">Phone:</label>
-          <input id="phone" type="text" input={phone} />
+          <input
+            id="phone"
+            type="text"
+            onChange={(e) => setPhone(e.target.value)}
+            value={phone}
+          />
         </div>
         <button>Submit</button>
       </form>
